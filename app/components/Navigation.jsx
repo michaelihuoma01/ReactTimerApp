c10c37892d5db29500bc06d444e09b546fff26cc
Michael <michaelihuoma01@gmail.com>
2019-03-26 06:38:19 +0100

    Initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Timer App</li>
+          <li>
+            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/Countdown" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by <a href="https://github.com/michaelihuoma01"  target="_blank">Michael Ihuoma</a>
+            </li>
+          </ul>
+      </div>
+    </div>
+  )
+};
+
+module.exports = Navigation;
